f7fc838944c89f4ac2bb367c32cdaf3a4e9461f4
Ricieri Sperandio <ricierisperandio@hotmail.com>
2022-11-11 21:31:56 -0300

    Configuraçãoes finais

--- src/test/java/one/digitalinnovation/parking/controller/ParkingControllerTest.java
+++ src/test/java/one/digitalinnovation/parking/controller/ParkingControllerTest.java
@@ -24,6 +24,7 @@ class ParkingControllerTest extends  AbstractContainerBase{
     @Test
     void whenFindAllThenCheckResult() {
         RestAssured.given()
+                .auth().basic("user", "Dio@123456")
                 .when()
                 .get("/parking")
                 .then()
@@ -42,6 +43,7 @@ class ParkingControllerTest extends  AbstractContainerBase{
 
         RestAssured.given()
                 .when()
+                .auth().basic("user", "Dio@123456")
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(createDTO)
                 .post("/parking")
